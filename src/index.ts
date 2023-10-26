127b7df63ad6ddc2846e8c5defee390ebfbb0b51
HaudinFlorence <haudin.florence@gmail.com>
2023-10-26 22:59:56 +0200

    Add a button in the toolbar of the notebook.

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,16 @@ import {
 } from '@jupyterlab/application';
 
 import { requestAPI } from './handler';
+import { IFileBrowserFactory } from '@jupyterlab/filebrowser';
+import { ITranslator } from '@jupyterlab/translation';
+import { addJupyterLabThemeChangeListener } from '@jupyter/web-components';
+import { Dialog, showDialog } from '@jupyterlab/apputils';
+import { DriveListModel, DriveListView } from './drivelistmanager';
+import { DriveIcon } from './icons';
+
+namespace CommandIDs {
+  export const openDrivesDialog = 'drives:open-drives-dialog';
+}
 
 /**
  * Initialization data for the @jupyter/drives extension.
@@ -27,4 +37,49 @@ const plugin: JupyterFrontEndPlugin<void> = {
   }
 };
 
-export default plugin;
+const openDriveDialogPlugin: JupyterFrontEndPlugin<void> = {
+  id: '@jupyter/drives:widget',
+  description: 'Open a dialog to select drives to be added in the filebrowser.',
+  requires: [IFileBrowserFactory, ITranslator],
+  autoStart: true,
+  activate: (
+    app: JupyterFrontEnd,
+    factory: IFileBrowserFactory,
+    translator: ITranslator
+  ): void => {
+    addJupyterLabThemeChangeListener();
+    const { commands } = app;
+    const { tracker } = factory;
+    const trans = translator.load('jupyter_drives');
+
+    commands.addCommand(CommandIDs.openDrivesDialog, {
+      execute: args => {
+        const widget = tracker.currentWidget;
+        const driveList = [
+          'BananaDrive',
+          'RaspberryDrive',
+          'PineAppleDrive',
+          'PomeloDrive',
+          'OrangeDrive',
+          'TomatoDrive',
+          'AvocadoDrive'
+        ];
+
+        const model = new DriveListModel(driveList);
+
+        if (widget) {
+          showDialog({
+            body: new DriveListView(model),
+            buttons: [Dialog.cancelButton()]
+          });
+        }
+      },
+
+      icon: DriveIcon.bindprops({ stylesheet: 'menuItem' }),
+      caption: trans.__('Add drives to filebrowser.'),
+      label: trans.__('Add Drives To Filebrowser')
+    });
+  }
+};
+const plugins: JupyterFrontEndPlugin<any>[] = [plugin, openDriveDialogPlugin];
+export default plugins;
