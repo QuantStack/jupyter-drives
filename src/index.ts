0e0d0377e8772630b800af0b91beb3063957bbdc
HaudinFlorence <haudin.florence@gmail.com>
2023-11-09 17:35:28 +0100

    Merge the dialog and the filebrowser plugins into one single called AddDrivesPlugin and moved hard coded inputs mimicking backend side inputs , outside from the plugins and methods.

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import {
   ILayoutRestorer,
   JupyterFrontEnd,
   JupyterFrontEndPlugin
-  //ILabShell,
-  //IRouter
 } from '@jupyterlab/application';
 
 import { IFileBrowserFactory } from '@jupyterlab/filebrowser';
@@ -13,11 +11,81 @@ import { Dialog, showDialog } from '@jupyterlab/apputils';
 import { DriveListModel, DriveListView, IDrive } from './drivelistmanager';
 import { DriveIcon } from './icons';
 import { IDocumentManager } from '@jupyterlab/docmanager';
-import { ISettingRegistry } from '@jupyterlab/settingregistry';
 import { Drive } from './contents';
 import { DefaultAndDrivesFileBrowser } from './browser';
 
-const PLUGIN_ID = '@jupyterlab/jupyter:drives';
+const selectedList1 = [
+  {
+    name: 'CoconutDrive',
+    url: '/coconut/url'
+  }
+];
+
+const availableList1 = [
+  {
+    name: 'CoconutDrive',
+    url: '/coconut/url'
+  },
+  {
+    name: 'PeachDrive',
+    url: '/peach/url'
+  },
+  {
+    name: 'WaterMelonDrive',
+    url: '/WaterMelonDrive/url'
+  },
+  {
+    name: 'MangoDrive',
+    url: '/mango/url'
+  },
+  {
+    name: 'KiwiDrive',
+    url: '/kiwi/url'
+  },
+  {
+    name: 'PearDrive',
+    url: '/pear/url'
+  },
+  {
+    name: 'StrawberryDrive',
+    url: '/strawberrydrive/url'
+  },
+  {
+    name: 'BlueberryDrive',
+    url: '/blueberrydrive/url'
+  },
+  {
+    name: '',
+    url: '/mydrive/url'
+  },
+  {
+    name: 'RaspberryDrive',
+    url: '/raspberrydrive/url'
+  },
+
+  {
+    name: 'PineAppleDrive',
+    url: ''
+  },
+
+  { name: 'PomeloDrive', url: '/https://pomelodrive/url' },
+  {
+    name: 'OrangeDrive',
+    url: ''
+  },
+  {
+    name: 'TomatoDrive',
+    url: ''
+  },
+  {
+    name: '',
+    url: 'superDrive/url'
+  },
+  {
+    name: 'AvocadoDrive',
+    url: ''
+  }
+];
 
 namespace CommandIDs {
   export const openDrivesDialog = 'drives:open-drives-dialog';
@@ -36,144 +104,32 @@ const plugin: JupyterFrontEndPlugin<void> = {
   }
 };
 
-/**
- * The JupyterLab plugin for the Drives Filebrowser.
- */
-const driveFileBrowserPlugin: JupyterFrontEndPlugin<void> = {
-  id: PLUGIN_ID,
-  requires: [IDocumentManager, IFileBrowserFactory, ISettingRegistry],
-  optional: [ILayoutRestorer],
-  activate: activateFileBrowser,
-  autoStart: true
-};
-
-/**
- * Activate the file browser.
- */
-function activateFileBrowser(
-  app: JupyterFrontEnd,
-  manager: IDocumentManager,
-  factory: IFileBrowserFactory,
-  restorer: ILayoutRestorer | null
-): void {
-  const panel = new DefaultAndDrivesFileBrowser();
-  const addedDrive = new Drive(app.docRegistry);
-  addedDrive.name = 'mydrive1';
-  manager.services.contents.addDrive(addedDrive);
-
-  const defaultBrowser = factory.createFileBrowser('default-browser', {
-    refreshInterval: 300000
-  });
-
-  const driveBrowser = factory.createFileBrowser('drive-browser', {
-    driveName: addedDrive.name,
-    refreshInterval: 300000
-  });
-  panel.addWidget(defaultBrowser);
-  panel.addWidget(driveBrowser);
-
-  panel.title.icon = DriveIcon;
-  panel.title.iconClass = 'jp-SideBar-tabIcon';
-  panel.title.caption = 'Browse Drives';
-
-  panel.id = 'drive-file-browser';
-
-  // Add the file browser widget to the application restorer.
-  if (restorer) {
-    restorer.add(panel, 'drive-browser');
-  }
-
-  app.shell.add(panel, 'left', { rank: 102 });
-}
-
-const openDriveDialogPlugin: JupyterFrontEndPlugin<void> = {
-  id: '@jupyter/drives:widget',
+const AddDrivesPlugin: JupyterFrontEndPlugin<void> = {
+  id: '@jupyterlab/jupyter:drives',
   description: 'Open a dialog to select drives to be added in the filebrowser.',
-  requires: [IFileBrowserFactory, ITranslator],
+  requires: [
+    IFileBrowserFactory,
+    IDocumentManager,
+    ITranslator,
+    ILayoutRestorer
+  ],
   autoStart: true,
   activate: (
     app: JupyterFrontEnd,
     factory: IFileBrowserFactory,
-    translator: ITranslator
+    manager: IDocumentManager,
+    translator: ITranslator,
+    restorer: ILayoutRestorer | null
   ): void => {
-    addJupyterLabThemeChangeListener();
     const { commands } = app;
     const { tracker } = factory;
     const trans = translator.load('jupyter_drives');
+
+    /* Dialog to select the drive */
+    addJupyterLabThemeChangeListener();
     const selectedDrivesModelMap = new Map<IDrive[], DriveListModel>();
-
-    let selectedDrives: IDrive[] = [
-      {
-        name: 'CoconutDrive',
-        url: '/coconut/url'
-      }
-    ];
-
-    const availableDrives: IDrive[] = [
-      {
-        name: 'CoconutDrive',
-        url: '/coconut/url'
-      },
-      {
-        name: 'PeachDrive',
-        url: '/peach/url'
-      },
-      {
-        name: 'WaterMelonDrive',
-        url: '/WaterMelonDrive/url'
-      },
-      {
-        name: 'MangoDrive',
-        url: '/mango/url'
-      },
-      {
-        name: 'KiwiDrive',
-        url: '/kiwi/url'
-      },
-      {
-        name: 'PearDrive',
-        url: '/pear/url'
-      },
-      {
-        name: 'StrawberryDrive',
-        url: '/strawberrydrive/url'
-      },
-      {
-        name: 'BlueberryDrive',
-        url: '/blueberrydrive/url'
-      },
-      {
-        name: '',
-        url: '/mydrive/url'
-      },
-      {
-        name: 'RaspberryDrive',
-        url: '/raspberrydrive/url'
-      },
-
-      {
-        name: 'PineAppleDrive',
-        url: ''
-      },
-
-      { name: 'PomeloDrive', url: '/https://pomelodrive/url' },
-      {
-        name: 'OrangeDrive',
-        url: ''
-      },
-      {
-        name: 'TomatoDrive',
-        url: ''
-      },
-      {
-        name: '',
-        url: 'superDrive/url'
-      },
-      {
-        name: 'AvocadoDrive',
-        url: ''
-      }
-    ];
+    let selectedDrives: IDrive[] = selectedList1;
+    const availableDrives: IDrive[] = availableList1;
     let model = selectedDrivesModelMap.get(selectedDrives);
 
     commands.addCommand(CommandIDs.openDrivesDialog, {
@@ -187,11 +143,11 @@ const openDriveDialogPlugin: JupyterFrontEndPlugin<void> = {
           selectedDrives = model.selectedDrives;
           selectedDrivesModelMap.set(selectedDrives, model);
         }
+
         if (widget) {
           if (model) {
             showDialog({
               body: new DriveListView(model),
-
               buttons: [Dialog.cancelButton()]
             });
           }
@@ -202,12 +158,34 @@ const openDriveDialogPlugin: JupyterFrontEndPlugin<void> = {
       caption: trans.__('Add drives to filebrowser.'),
       label: trans.__('Add Drives To Filebrowser')
     });
+
+    /* Add a left panel containing the default filebrowser and a dedicated browser for the selected drive*/
+    const panel = new DefaultAndDrivesFileBrowser();
+    const defaultBrowser = factory.createFileBrowser('default-browser', {
+      refreshInterval: 300000
+    });
+
+    const addedDrive = new Drive(app.docRegistry);
+    addedDrive.name = 'mydrive1';
+    manager.services.contents.addDrive(addedDrive);
+    const driveBrowser = factory.createFileBrowser('drive-browser', {
+      driveName: addedDrive.name,
+      refreshInterval: 300000
+    });
+    panel.addWidget(defaultBrowser);
+    panel.addWidget(driveBrowser);
+
+    panel.title.icon = DriveIcon;
+    panel.title.iconClass = 'jp-SideBar-tabIcon';
+    panel.title.caption = 'Browse Drives';
+    panel.id = 'panel-file-browser';
+
+    if (restorer) {
+      restorer.add(panel, 'drive-browser');
+    }
+
+    app.shell.add(panel, 'left', { rank: 102 });
   }
 };
-const plugins: JupyterFrontEndPlugin<any>[] = [
-  plugin,
-  openDriveDialogPlugin,
-  //defaultFileBrowser
-  driveFileBrowserPlugin
-];
+const plugins: JupyterFrontEndPlugin<any>[] = [plugin, AddDrivesPlugin];
 export default plugins;
