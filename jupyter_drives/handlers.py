1e8cd9f973eb42419f10ecf8e253020b0389f78d
DenisaCG <denisa.checiu@gmail.com>
2023-11-14 12:36:50 +0100

    remove example handler class

--- jupyter_drives/handlers.py
+++ jupyter_drives/handlers.py
@@ -42,16 +42,6 @@ class JupyterDrivesAPIHandler(APIHandler):
                 reply["error"] = "".join(traceback.format_exception(*exc_info))
         self.finish(json.dumps(reply))
 
-class ExampleJupyterDrivesHandler(JupyterDrivesAPIHandler):
-    # The following decorator should be present on all verb methods (head, get, post,
-    # patch, put, delete, options) to ensure only authorized user can request the
-    # Jupyter server
-    @tornado.web.authenticated
-    def get(self):
-        self.finish(json.dumps({
-            "data": "This is /jupyter-drives/get-example endpoint!"
-        }))
-
 class ListJupyterDrives(JupyterDrivesAPIHandler):
     """
     Returns list of available drives.
