87b5e79eb6220c12e48af839d94241e58a9cec2a
DenisaCG <denisa.checiu@gmail.com>
2023-11-20 16:46:20 +0100

    add tests

--- jupyter_drives/tests/test_handlers.py
+++ jupyter_drives/tests/test_handlers.py
@@ -25,7 +25,7 @@ def set_s3_base():
     server.stop()
 
 @pytest.fixture
-async def test_ListJupyterDrives(jp_fetch, set_s3_base):
+async def test_ListJupyterDrives_s3_success(jp_fetch, set_s3_base):
     with mock_s3(): 
         S3Drive = get_driver(Provider.S3)
         drive = S3Drive('access_key', 'secret_key')
@@ -38,10 +38,44 @@ async def test_ListJupyterDrives(jp_fetch, set_s3_base):
         drive.create_container(test_bucket_name_2)
 
         # When
-        response = await jp_fetch("jupyter-drives", "get-listDrives")
+        response = await jp_fetch("jupyter-drives", "drives")
 
         # Then
         assert response.code == 200
         payload = json.loads(response.body)
         assert "jupyter-drives-test-bucket-1" in payload
         assert "jupyter-drives-test-bucket-2" in payload
+
+@pytest.fixture
+async def test_ListJupyterDrives_s3_empty_list(jp_fetch, set_s3_base):
+    with mock_s3(): 
+        S3Drive = get_driver(Provider.S3)
+        drive = S3Drive('access_key', 'secret_key')
+
+        # When
+        response = await jp_fetch("jupyter-drives", "drives")
+
+        # Then
+        assert response.code == 200
+        payload = json.loads(response.body)
+        assert payload.len == 0 
+
+@pytest.fixture
+async def test_ListJupyterDrives_s3_missing_credentials(jp_fetch, set_s3_base):
+    with mock_s3(): 
+        S3Drive = get_driver(Provider.S3)
+        drive = S3Drive()
+
+        test_bucket_name_1 = "jupyter-drives-test-bucket-1"
+        test_bucket_name_2 = "jupyter-drives-test-bucket-2"
+
+        # Create a test container
+        drive.create_container(test_bucket_name_1)
+        drive.create_container(test_bucket_name_2)
+
+        # When
+        response = await jp_fetch("jupyter-drives", "drives")
+
+        # Then
+        error = json.loads(response.body)
+        assert error == "No AWS credentials provided."
