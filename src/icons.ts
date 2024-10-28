778c62b35695003ed540cbcdbd0161f06c11306f
DenisaCG <denisa.checiu@gmail.com>
2024-10-28 17:10:07 +0100

    instate drive file browser

--- src/icons.ts
+++ src/icons.ts
@@ -1,6 +1,13 @@
 import { LabIcon } from '@jupyterlab/ui-components';
 import driveSvgstr from '../style/drive.svg';
+import driveBrowserSvg from '../style/driveIconFileBrowser.svg';
+
 export const DriveIcon = new LabIcon({
-  name: '@jupyter/drives:drive',
+  name: 'jupyter-drives:drive',
   svgstr: driveSvgstr
+});
+
+export const driveBrowserIcon = new LabIcon({
+  name: 'jupyter-drives:drive-browser',
+  svgstr: driveBrowserSvg
 });
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
 import {
+  ILabShell,
+  ILayoutRestorer,
+  IRouter,
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-
-import { IFileBrowserFactory } from '@jupyterlab/filebrowser';
+import { IFileBrowserFactory, FileBrowser } from '@jupyterlab/filebrowser';
 import { ITranslator } from '@jupyterlab/translation';
 import { addJupyterLabThemeChangeListener } from '@jupyter/web-components';
 import { Dialog, showDialog } from '@jupyterlab/apputils';
-import { DriveListModel, DriveListView, IDrive } from './drivelistmanager';
-import { DriveIcon } from './icons';
+import { CommandRegistry } from '@lumino/commands';
+import { Panel } from '@lumino/widgets';
 
+import { DriveListModel, DriveListView, IDrive } from './drivelistmanager';
+import { DriveIcon, driveBrowserIcon } from './icons';
+
+/**
+ * The command IDs used by the driveBrowser plugin.
+ */
 namespace CommandIDs {
   export const openDrivesDialog = 'drives:open-drives-dialog';
+  export const openPath = 'filebrowser:open-path';
+  export const toggleBrowser = 'filebrowser:toggle-main';
 }
 
 /**
@@ -129,5 +139,126 @@ const openDriveDialogPlugin: JupyterFrontEndPlugin<void> = {
     });
   }
 };
-const plugins: JupyterFrontEndPlugin<any>[] = [plugin, openDriveDialogPlugin];
+
+/**
+ * The drive file browser factory provider.
+ */
+const driveFileBrowser: JupyterFrontEndPlugin<void> = {
+  id: 'jupyter-drives:drive-file-browser',
+  description: 'The drive file browser factory provider.',
+  autoStart: true,
+  requires: [IFileBrowserFactory],
+  optional: [
+    IRouter,
+    JupyterFrontEnd.ITreeResolver,
+    ILabShell,
+    ILayoutRestorer
+  ],
+  activate: async (
+    app: JupyterFrontEnd,
+    fileBrowserFactory: IFileBrowserFactory,
+    router: IRouter | null,
+    tree: JupyterFrontEnd.ITreeResolver | null,
+    labShell: ILabShell | null,
+    restorer: ILayoutRestorer | null
+  ): Promise<void> => {
+    const { commands } = app;
+
+    // create S3 drive
+    // const S3Drive = new Drive({
+    //   name: auth.bucket,
+    //   root: auth.root,
+    //   config: auth.config
+    // });
+
+    // app.serviceManager.contents.addDrive(S3Drive);
+
+    // Manually restore and load the drive file browser.
+    const driveBrowser = fileBrowserFactory.createFileBrowser('drivebrowser', {
+      auto: false,
+      restore: false
+      // driveName: S3Drive.name
+    });
+
+    // // Set attributes when adding the browser to the UI
+    driveBrowser.node.setAttribute('role', 'region');
+    driveBrowser.node.setAttribute('aria-label', 'Drive Browser Section');
+
+    // instate Drive Browser Panel
+    const drivePanel = new Panel();
+    drivePanel.title.icon = driveBrowserIcon;
+    drivePanel.title.iconClass = 'jp-sidebar-tabIcon';
+    drivePanel.title.caption = 'Drive FileBrowser';
+    drivePanel.id = 'Drive-Browser-Panel';
+
+    app.shell.add(drivePanel, 'left', { rank: 102 });
+    // drivePanel.addWidget(driveBrowser);
+    if (restorer) {
+      restorer.add(drivePanel, 'drive-sidepanel');
+    }
+
+    void Private.restoreBrowser(driveBrowser, commands, router, tree, labShell);
+  }
+};
+
+const plugins: JupyterFrontEndPlugin<any>[] = [
+  plugin,
+  driveFileBrowser,
+  openDriveDialogPlugin
+];
 export default plugins;
+
+namespace Private {
+  /**
+   * Restores file browser state and overrides state if tree resolver resolves.
+   */
+  export async function restoreBrowser(
+    browser: FileBrowser,
+    commands: CommandRegistry,
+    router: IRouter | null,
+    tree: JupyterFrontEnd.ITreeResolver | null,
+    labShell: ILabShell | null
+  ): Promise<void> {
+    const restoring = 'jp-mod-restoring';
+
+    browser.addClass(restoring);
+
+    if (!router) {
+      await browser.model.restore(browser.id);
+      await browser.model.refresh();
+      browser.removeClass(restoring);
+      return;
+    }
+
+    const listener = async () => {
+      router.routed.disconnect(listener);
+
+      const paths = await tree?.paths;
+      if (paths?.file || paths?.browser) {
+        // Restore the model without populating it.
+        await browser.model.restore(browser.id, false);
+        if (paths.file) {
+          await commands.execute(CommandIDs.openPath, {
+            path: paths.file,
+            dontShowBrowser: true
+          });
+        }
+        if (paths.browser) {
+          await commands.execute(CommandIDs.openPath, {
+            path: paths.browser,
+            dontShowBrowser: true
+          });
+        }
+      } else {
+        await browser.model.restore(browser.id);
+        await browser.model.refresh();
+      }
+      browser.removeClass(restoring);
+
+      if (labShell?.isEmpty('main')) {
+        void commands.execute('launcher:create');
+      }
+    };
+    router.routed.connect(listener);
+  }
+}
