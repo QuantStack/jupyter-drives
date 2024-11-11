b6e30dc0257dc2a00f60d8870e2a22b94607391f
DenisaCG <denisa.checiu@gmail.com>
2024-11-11 12:03:06 +0100

    add docstrings

--- src/token.ts
+++ src/token.ts
@@ -1,9 +1,15 @@
 import { Token } from '@lumino/coreutils';
 
+/**
+ * A token for the plugin that provides the list of drives.
+ */
 export const IDrivesList = new Token<IDriveInfo[]>(
   '@jupyter/drives:drives-list-provider'
 );
 
+/**
+ * An interface for the available drives.
+ */
 export interface IDriveInfo {
   name: string;
   region: string;
