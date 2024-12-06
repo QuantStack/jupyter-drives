0909ca7f7c42dde5dee9343d2d7dc472fb23839c
DenisaCG <denisa.checiu@gmail.com>
2024-12-06 16:49:26 +0100

    remove limit of listing from config

--- jupyter_drives/base.py
+++ jupyter_drives/base.py
@@ -59,13 +59,6 @@ class DrivesConfig(Configurable):
         help="Custom path of file where credentials are located. Extension automatically checks jupyter_notebook_config.py or directly in ~/.aws/credentials for AWS CLI users."
     )
 
-    max_files_listed = Int(
-        None,
-        config = True,
-        allow_none = True,
-        help="The maximum number of files that will show inside the drive browser, given any path."
-    )
-
     @default("api_base_url")
     def set_default_api_base_url(self):
         # for AWS S3 drives
@@ -87,14 +80,7 @@ class DrivesConfig(Configurable):
         super().__init__(**kwargs)
         self._load_credentials()
     
-    def _load_credentials(self):
-        # check if max_files_shown is not set in jupyter_notebook_config.py
-        if self.max_files_listed is None:
-            if "JP_DRIVES_MAX_FILES_LISTED" in os.environ:
-                self.max_files_listed = os.environ["JP_DRIVES_MAX_FILES_LISTED"]
-            else:
-                self.max_files_listed = 1000
-        
+    def _load_credentials(self):        
         # check if credentials were already set in jupyter_notebook_config.py
         if self.access_key_id is not None and self.secret_access_key is not None:
             return
@@ -103,7 +89,7 @@ class DrivesConfig(Configurable):
         if self.custom_credentials_path is None and "JP_DRIVES_CUSTOM_CREDENTIALS_PATH" in os.environ:
             self.custom_credentials_path = os.environ["JP_DRIVES_CUSTOM_CREDENTIALS_PATH"]
         if self.custom_credentials_path is not None:
-            self.provider, self.access_key_id, self.secret_access_key, self.session_token, self.max_files_listed = self._extract_credentials_from_file(self.custom_credentials_path)
+            self.provider, self.access_key_id, self.secret_access_key, self.session_token = self._extract_credentials_from_file(self.custom_credentials_path)
             return
         
         # if not, try to load credentials from AWS CLI
@@ -126,7 +112,6 @@ class DrivesConfig(Configurable):
         try:
             with open(file_path, 'r') as file:
                 provider, access_key_id, secret_access_key, session_token = None, None, None, None
-                max_files_listed = 1000
                 lines = file.readlines()
                 for line in lines:
                     if line.startswith("drives_provider ="):
@@ -137,9 +122,7 @@ class DrivesConfig(Configurable):
                         secret_access_key = line.split("=")[1].strip()
                     elif line.startswith("drives_session_token ="):
                         session_token = line.split("=")[1].strip()
-                    elif line.startswith("drives_max_files_shwon ="):
-                        max_files_listed = line.split("=")[1].strip()
-                return provider, access_key_id, secret_access_key, session_token, max_files_listed
+                return provider, access_key_id, secret_access_key, session_token
         except Exception as e:
             print(f"Failed loading credentials from {file_path}: {e}")
         return
